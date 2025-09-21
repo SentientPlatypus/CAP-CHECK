0495abf1d5077ebfa55465c57143f648f5073df7
Zenchang Sun <zs434@cornell.edu>
2025-09-20 20:26:19 -0400

    f

--- backend/app.py
+++ backend/app.py
@@ -125,4 +125,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     threading.Thread(target=launch_frontend, daemon=True).start()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
